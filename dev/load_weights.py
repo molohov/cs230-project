ea28c66c74237332662476b69ec0af06e8a7d49d
I-Sheng Yang <yang.isheng@me.com>
2019-03-10 13:06:37 -0700

    remove saved weights from commit

---
+++ dev/load_weights.py
@@ -0,0 +1,72 @@
+import numpy as np
+import keras.backend as K
+from keras import layers
+from keras.applications.imagenet_utils import preprocess_input
+from keras.applications.inception_v3 import InceptionV3, preprocess_input, decode_predictions
+from keras.callbacks import ModelCheckpoint, CSVLogger, LearningRateScheduler, ReduceLROnPlateau
+from keras.layers import AveragePooling2D, MaxPooling2D, Dropout, GlobalMaxPooling2D, GlobalAveragePooling2D, Input, BatchNormalization, Conv2D
+from keras.layers import Dense, Activation, Flatten, Convolution2D, MaxPooling2D, ZeroPadding2D, GlobalAveragePooling2D, AveragePooling2D
+from keras.layers.normalization import BatchNormalization
+from keras.models import Sequential, Model
+from keras.optimizers import SGD, Adam
+from keras.preprocessing import image
+from keras.preprocessing.image import ImageDataGenerator
+from keras.regularizers import l2
+from keras.utils import layer_utils, plot_model
+from keras.utils.data_utils import get_file
+from keras.utils.vis_utils import model_to_dot
+from os import makedirs
+from os.path import isdir
+import pydot
+from IPython.display import SVG
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import imshow
+from load_dataset import load_dataset
+import math
+import h5py
+# import parallelTestModule
+import json
+import master_config
+from keras.callbacks import ModelCheckpoint
+from DataGenerator import DataGenerator
+
+# create_model
+#
+# Create the neural network model
+def create_model(num_classes=master_config.params['n_classes'], l2_regularizer = 0.05):
+    ## load in inceptionv3 model
+    K.clear_session()
+    base_model = InceptionV3(weights='imagenet', include_top=False, input_tensor=Input(shape=(master_config.height, master_config.width, master_config.num_channels)), pooling='max')
+
+    if master_config.freeze_base_model:
+        for layer in base_model.layers:
+            layer.trainable = False
+
+    # Custom layers after base model's output
+    x = base_model.output
+    x = Dense(num_classes, kernel_initializer='glorot_uniform', kernel_regularizer=l2(l2_regularizer), activation='softmax')(x)
+
+    model = Model(inputs=base_model.input, outputs=x)
+    return model
+
+
+# # Datasets
+# partition = # IDs
+# labels = # Labels
+
+# Generators
+def main():
+    # Design model
+    model = create_model(master_config.params['n_classes'])
+
+    save_weight_filepath = master_config.model_save_path + "/weights -  28 -  0.5993.hdf5"
+
+    model.load_weights(save_weight_filepath)
+
+    for layer in model.layers:
+        weights = layer.get_weights() # list of numpy arrays
+        print (weights)
+
+if __name__ == "__main__":
+    main()
+
